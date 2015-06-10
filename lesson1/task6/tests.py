677d107741424b023c0406a2c21fe04e9d1969d0
theakaterina <theakoutsoukis@gmail.com>
2015-06-10 13:28:34 +1000

    Second draft of lessons - fixed lesson 1 task 6 (repeating)

--- lesson1/task6/tests.py
+++ lesson1/task6/tests.py
@@ -5,14 +5,14 @@ def test_answer_placeholders():
     answers = get_file_output()
     hey_jude = answers[0]
     batman = answers[1]
-    if hey_jude == "na na na na na na na na na hey jude":
+    if hey_jude == "na na na na na na na na na na na hey jude":
         passed()
     else:
-        failed("Something wrong hey jude")
+        failed("Something's wrong with hey jude")
     if batman == "na na na na na na na na na na na na na na na na batman":
         passed()
     else:
-        failed("Something wrong with batman")
+        failed("Something's wrong with batman")
 
 if __name__ == '__main__':
     run_common_tests()
